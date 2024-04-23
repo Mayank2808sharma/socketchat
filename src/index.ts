7eabce2e956ca00f9ce1607047fbf21ae2707851
mayank2808sharma <mayank2808sharma@gmail.com>
2024-04-23 19:23:59 +0530

    socket is connected

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,35 @@
-import express,{Request,Response } from 'express';
-import { createServer } from 'http';
-import cors from 'cors';
+import express, { Request, Response } from "express";
+import { createServer } from "http";
+import cors from "cors";
+import { Server } from "socket.io";
+
 const app = express();
 const server = createServer(app);
+
+// socket io server
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
+});
 app.use(cors());
-app.get('/', (req:Request, res:Response) => {
-    res.send('<h1>Hello world</h1>');
+app.get("/", (req: Request, res: Response) => {
+  res.send("<h1>Hello world</h1>");
+});
+
+io.on("connection", (socket) => {
+  console.log(`user is connected ${socket.id}`);
+
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
   });
-  
-  server.listen(3000, () => {
-    console.log('server running at http://localhost:3000');
-  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  })
+});
+
+server.listen(3000, () => {
+  console.log("server running at http://localhost:3000");
+});
